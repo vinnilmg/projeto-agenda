0ba9ef0ab90e8843604f6f7fbcb7137e3de6e050
Vinicius de Lima Gomes <vinicius.gomes@sulamerica.com.br>
2021-09-24 19:39:44 -0300

    migration, ifs, paginacao

--- contatos/views.py
+++ contatos/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Contato
 
 # Create your views here.
 
 
 def index(request):
-
     contatos = Contato.objects.all()
+    paginator = Paginator(contatos, 5)
+
+    paginas = request.GET.get('p')
+    contatos = paginator.get_page(paginas)
 
     return render(request, 'contatos/index.html', {
         'contatos': contatos
@@ -14,7 +18,8 @@ def index(request):
 
 
 def ver_contato(request, contato_id):
-    contato = Contato.objects.get(id=contato_id)
+    # contato = Contato.objects.get(id=contato_id)
+    contato = get_object_or_404(Contato, id=contato_id)
 
     return render(request, 'contatos/visualiza_contato.html', {
         'contato': contato
